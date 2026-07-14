1bcaa1bd968196569daa8ca47620cdebe15f4f58
Ahmed-Khaled-Abdelmaksod <ak4902280@gmail.com>
2026-07-14 15:12:44 +0300

    feat: add dummy security

---
+++ src/main/java/gov/jets/iti/LinguaQuest/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package gov.jets.iti.LinguaQuest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    SecurityFilterChain customizeFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(c -> c.disable())
+                .formLogin(f->f.disable())
+                .authorizeHttpRequests(a ->
+                        a.anyRequest().permitAll())
+                .build();
+    }
+}
