c06106288dbafb5b318ed7fd38b9b313568eca62
Ahmed Abdelrahman <mizobek74@gmail.com>
2026-07-23 05:45:03 +0300

    Add new query methods in UserLevelProgressRepository and WorldLevelRepository

--- src/main/java/gov/jets/iti/LinguaQuest/repository/WorldLevelRepository.java
+++ src/main/java/gov/jets/iti/LinguaQuest/repository/WorldLevelRepository.java
@@ -4,11 +4,15 @@ import gov.jets.iti.LinguaQuest.entity.World;
 import gov.jets.iti.LinguaQuest.entity.WorldLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorldLevelRepository extends JpaRepository<WorldLevel,Long> {
     long countWorldLevelByWorld(World world);
+
+    Optional<WorldLevel> findByIdAndWorldId(Long id, Long worldId);
 
     @Query("""
         SELECT wl
