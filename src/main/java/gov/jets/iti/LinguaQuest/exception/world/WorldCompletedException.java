65c1f5ebb3b8afbd98be05ca786d9e3847c1b706
Ahmed Abdelrahman <mizobek74@gmail.com>
2026-07-23 05:01:33 +0300

    Add LevelLockedException, LevelNotFoundException, and WorldCompletedException classes

---
+++ src/main/java/gov/jets/iti/LinguaQuest/exception/world/WorldCompletedException.java
@@ -0,0 +1,7 @@
+package gov.jets.iti.LinguaQuest.exception.world;
+
+public class WorldCompletedException extends RuntimeException {
+    public WorldCompletedException(String message) {
+        super(message);
+    }
+}
