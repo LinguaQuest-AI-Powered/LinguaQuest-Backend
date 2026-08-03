f58d9638e6f83265ee25cc8031a674c191a98f14
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2026-08-03 18:40:11 +0300

    add NotificationNotFoundException and handler for notification not found errors

---
+++ src/main/java/gov/jets/iti/LinguaQuest/exception/NotificationNotFoundException.java
@@ -0,0 +1,7 @@
+package gov.jets.iti.LinguaQuest.exception;
+
+public class NotificationNotFoundException extends RuntimeException {
+    public NotificationNotFoundException(String message) {
+        super(message);
+    }
+}
