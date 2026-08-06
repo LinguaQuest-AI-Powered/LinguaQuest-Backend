0058d8bd7c1fcd73f41af61de939bce092cb41a0
Ahmed-Khaled-Abdelmaksod <ak4902280@gmail.com>
2026-08-06 18:41:12 +0300

    feat: add user daily missions exceptions.

---
+++ src/main/java/gov/jets/iti/LinguaQuest/exception/DailyMissionWordNotFound.java
@@ -0,0 +1,7 @@
+package gov.jets.iti.LinguaQuest.exception;
+
+public class DailyMissionWordNotFound extends RuntimeException {
+    public DailyMissionWordNotFound(String message) {
+        super(message);
+    }
+}
